a52ef0d53af9b019717540bbab42d734f26e312c
Tomas Madariaga <critimass@gmail.com>
2025-03-10 17:20:57 +0100

    feat: add react-virtualized and eslint better rules

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect, useCallback, useRef, memo } from "react";
 import { trpcReact } from "@/trpc/trpcReact";
 import { Box, Typography, Button, TextField } from "@mui/material";
+import {
+  List,
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+} from "react-virtualized";
 
 const PostList: React.FC = () => {
   const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
@@ -26,7 +32,6 @@ const PostList: React.FC = () => {
   const [showComments, setShowComments] = useState<{ [key: number]: boolean }>(
     {}
   );
-  const observer = useRef<IntersectionObserver | null>(null);
 
   useEffect(() => {
     if (data) {
@@ -34,22 +39,10 @@ const PostList: React.FC = () => {
     }
   }, [data]);
 
-  const lastPostElementRef = useCallback(
-    (node) => {
-      if (isLoading || !hasNextPage) return;
-      if (observer.current) observer.current.disconnect();
-
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-          console.log("Last post is in view, fetching next page...");
-          fetchNextPage();
-        }
-      });
-
-      if (node) observer.current.observe(node);
-    },
-    [isLoading, hasNextPage, fetchNextPage]
-  );
+  const cache = new CellMeasurerCache({
+    fixedWidth: true,
+    defaultHeight: 200,
+  });
 
   const handleAddComment = (postId: number) => {
     if (comments[postId]) {
@@ -69,21 +62,64 @@ const PostList: React.FC = () => {
     }));
   };
 
+  const renderRow = ({ index, key, parent, style }) => {
+    const post = localPosts[index];
+    return (
+      <CellMeasurer
+        key={key}
+        cache={cache}
+        parent={parent}
+        columnIndex={0}
+        rowIndex={index}
+      >
+        {({ registerChild }) => (
+          <div ref={registerChild} style={style}>
+            <PostItem
+              post={post}
+              comment={comments[post.id] || ""}
+              showComments={showComments[post.id]}
+              onCommentChange={handleCommentChange}
+              onAddComment={handleAddComment}
+              onToggleComments={toggleComments}
+            />
+          </div>
+        )}
+      </CellMeasurer>
+    );
+  };
+
   return (
-    <Box display="flex" flexDirection="column" alignItems="center">
-      {localPosts?.map((post, index) => (
-        <PostItem
-          key={`post-${post.id}-${index}`}
-          post={post}
-          comment={comments[post.id] || ""}
-          showComments={showComments[post.id]}
-          onCommentChange={handleCommentChange}
-          onAddComment={handleAddComment}
-          onToggleComments={toggleComments}
-          ref={index === localPosts.length - 1 ? lastPostElementRef : null}
-        />
-      ))}
-      {isFetchingNextPage && <div>Loading more...</div>}
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      height="100vh"
+    >
+      <AutoSizer style={{ width: "100%" }}>
+        {({ height, width }) => (
+          <List
+            width={width}
+            height={height}
+            deferredMeasurementCache={cache}
+            rowHeight={cache.rowHeight}
+            rowRenderer={renderRow}
+            rowCount={localPosts.length}
+            overscanRowCount={3}
+            onRowsRendered={({ stopIndex }) => {
+              if (
+                stopIndex === localPosts.length - 1 &&
+                hasNextPage &&
+                !isFetchingNextPage
+              ) {
+                fetchNextPage();
+              }
+            }}
+          />
+        )}
+      </AutoSizer>
+      {isFetchingNextPage && (
+        <div style={{ position: "fixed", bottom: 10 }}>Loading more...</div>
+      )}
       {isLoading && <div>Loading...</div>}
     </Box>
   );
@@ -98,56 +134,48 @@ interface PostItemProps {
   onToggleComments: (postId: number) => void;
 }
 
-const PostItem = memo(
-  React.forwardRef<HTMLDivElement, PostItemProps>(
-    (
-      {
-        post,
-        comment,
-        showComments,
-        onCommentChange,
-        onAddComment,
-        onToggleComments,
-      },
-      ref
-    ) => {
-      return (
-        <Box ref={ref} border={1} padding={2} margin={2} width="50%">
-          <Typography variant="h5">{post.id}</Typography>
-          <Typography variant="h5">{post.title}</Typography>
-          <Typography variant="body1">{post.content}</Typography>
-          {post.comments.length > 0 && (
-            <Button
-              variant="contained"
-              onClick={() => onToggleComments(post.id)}
-            >
-              Comments ({post.comments.length})
-            </Button>
-          )}
-          {showComments && (
-            <Box>
-              {post.comments.map((comment) => (
-                <Typography
-                  key={`comment-${comment.id}-${post.id}`}
-                  variant="body2"
-                >
-                  {comment.content}
-                </Typography>
-              ))}
-            </Box>
-          )}
+const PostItem: React.FC<PostItemProps> = memo(
+  ({
+    post,
+    comment,
+    showComments,
+    onCommentChange,
+    onAddComment,
+    onToggleComments,
+  }) => {
+    return (
+      <Box border={1} padding={2} margin={2}>
+        <Typography variant="h5">{post.id}</Typography>
+        <Typography variant="h5">{post.title}</Typography>
+        <Typography variant="body1">{post.content}</Typography>
+        {post.comments.length > 0 && (
+          <Button variant="contained" onClick={() => onToggleComments(post.id)}>
+            Comments ({post.comments.length})
+          </Button>
+        )}
+        {showComments && (
           <Box>
-            <TextField
-              label="Add a comment"
-              value={comment}
-              onChange={(e) => onCommentChange(post.id, e.target.value)}
-            />
-            <Button onClick={() => onAddComment(post.id)}>Submit</Button>
+            {post.comments.map((comment: { id: number; content: string }) => (
+              <Typography
+                key={`comment-${comment.id}-${post.id}`}
+                variant="body2"
+              >
+                {comment.content}
+              </Typography>
+            ))}
           </Box>
+        )}
+        <Box>
+          <TextField
+            label="Add a comment"
+            value={comment}
+            onChange={(e) => onCommentChange(post.id, e.target.value)}
+          />
+          <Button onClick={() => onAddComment(post.id)}>Submit</Button>
         </Box>
-      );
-    }
-  )
+      </Box>
+    );
+  }
 );
 
 PostItem.displayName = "PostItem";
